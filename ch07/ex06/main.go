888da03eb5d437ba8adcfde8403051d1c5c46bf8
Hiroyuki Matsuo <h_matsuo@r.recruit.co.jp>
2020-10-30 16:48:05 +0900

    Fix 7.6

--- ch07/ex06/main.go
+++ ch07/ex06/main.go
@@ -1,27 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
 
-// import (
-// 	"flag"
-// 	"fmt"
+	"github.com/h-matsuo/gopl/ch07/ex06/tempconv"
+)
 
-// 	"github.com/h-matsuo/gopl/ch07/ex06/tempconv"
-// )
+var temp = tempconv.CelsiusFlag("temp", 20.0, "the temperature")
 
-// var temp = tempconv.CelsiusFlag("temp", 20.0, "the temperature")
-
-// func main() {
-// 	flag.Parse()
-// 	fmt.Println(*temp)
-// }
-
-type MyType struct{}
-
-func (m MyType) String() string {
-	return "hoge"
-}
 func main() {
-	var m MyType
-	fmt.Printf("%q\n", m)
+	flag.Parse()
+	fmt.Println(*temp)
 }
